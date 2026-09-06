4fa06d634d495a9ac67220047b2829c7aad822c6
Kaye Dacula <kaye.dacula@cit.edu>
2026-09-06 19:58:18 +0800

    Build quiz start screen

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main(){
+import 'start_screen.dart';
+
+void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(colors: [
-            const Color.fromARGB(255, 73, 7, 189),
-            const Color.fromARGB(255, 73, 6, 184),
-          ])
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
         ),
-        child: Center(
-          child: Text('Learn flutter the fun way')
-        ),
-      ),
       ),
     ),
   );
