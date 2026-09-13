19f6c05e77a9e22e874d81cd1f1bd2467c4ead8f
Kaye Dacula <kaye.dacula@cit.edu>
2026-09-13 18:38:27 +0800

    Style quiz summary items

--- quiz_app/lib/questions_summary/summary_item.dart
+++ quiz_app/lib/questions_summary/summary_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'question_identifier.dart';
 
@@ -12,32 +13,50 @@ class SummaryItem extends StatelessWidget {
     final isCorrectAnswer =
         itemData['user_answer'] == itemData['correct_answer'];
 
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        QuestionIdentifier(
-          isCorrectAnswer: isCorrectAnswer,
-          questionIndex: itemData['question_index'] as int,
-        ),
-        const SizedBox(width: 20),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                itemData['question'] as String,
-              ),
-              const SizedBox(height: 5),
-              Text(
-                itemData['user_answer'] as String,
-              ),
-              Text(
-                itemData['correct_answer'] as String,
-              ),
-            ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 8,
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          QuestionIdentifier(
+            isCorrectAnswer: isCorrectAnswer,
+            questionIndex: itemData['question_index'] as int,
           ),
-        ),
-      ],
+          const SizedBox(width: 20),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  itemData['question'] as String,
+                  style: GoogleFonts.lato(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  itemData['user_answer'] as String,
+                  style: const TextStyle(
+                    color: Color.fromARGB(255, 202, 171, 252),
+                  ),
+                ),
+                Text(
+                  itemData['correct_answer'] as String,
+                  style: const TextStyle(
+                    color: Color.fromARGB(255, 181, 254, 246),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
