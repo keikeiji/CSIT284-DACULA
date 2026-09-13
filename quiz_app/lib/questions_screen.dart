31dfd8dbabb7522c68f45c149ad402d3085a57f6
Kaye Dacula <kaye.dacula@cit.edu>
2026-09-13 14:12:36 +0800

    Pass selected answers between screens

--- quiz_app/lib/questions_screen.dart
+++ quiz_app/lib/questions_screen.dart
@@ -5,7 +5,12 @@ import 'answer_button.dart';
 import 'data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
-  const QuestionsScreen({super.key});
+  const QuestionsScreen({
+    super.key,
+    required this.onSelectAnswer,
+  });
+
+  final void Function(String answer) onSelectAnswer;
 
   @override
   State<QuestionsScreen> createState() {
@@ -16,7 +21,8 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   var currentQuestionIndex = 0;
 
-  void answerQuestion() {
+  void answerQuestion(String selectedAnswer) {
+    widget.onSelectAnswer(selectedAnswer);
     setState(() {
       currentQuestionIndex++;
     });
@@ -47,9 +53,11 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             ...currentQuestion.getShuffledAnswers().map((answer) {
               return AnswerButton(
                 answerText: answer,
-                onTap: answerQuestion,
+                 onTap: () {
+                  answerQuestion(answer);
+                },
               );
-            }),
+            })
           ],
         ),
       ),
